b3940473c104d10a563d38d0cdd7868eab100323
wismaeka7 <67813045+wismaeka@users.noreply.github.com>
2020-08-28 00:50:06 +0800

    refactoring product and controller

--- models/user.js
+++ models/user.js
@@ -26,6 +26,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     username: {
       type: DataTypes.STRING,
+      unique :true,
       validate: {
         notEmpty: {
           args: true,
--- routers/productRoute.js
+++ routers/productRoute.js
@@ -3,5 +3,6 @@ const route = require('express').Router();
 const ProductController = require('../controllers/productController.js');
 
 route.get('/', ProductController.showProduct)
+route.post('/add', ProductController.postAddProduct)
 
 module.exports = route
