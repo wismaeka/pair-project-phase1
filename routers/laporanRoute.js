5c35eb784d561ce89caf1258d288dc2c0bf7bebe
Rifkalagatha <mrifkalagatha@gmail.com>
2020-08-27 23:46:05 +0700

    finish laporan

--- routers/laporanRoute.js
+++ routers/laporanRoute.js
@@ -2,6 +2,6 @@ const LaporanController = require('../controllers/laporanController');
 const route = require('express').Router();
 
 route.get('/', LaporanController.viewLaporan)
-// route.get('/:id', LaporanController.laporanProduct)
+route.get('/:id', LaporanController.laporanProduct)
 
 module.exports = route
